425a600d0ff1e975a4fb92e71de801dfe9f628db
Jason <70564653+BioxMech@users.noreply.github.com>
2022-09-11 17:33:22 +0800

    Feature/date time form (#2)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import Home from "../pages/index";
+import Form from '../components/form.component';
 import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, renderHook, screen } from "@testing-library/react";
 
 describe("Main Page", () => {
   it("renders the Main Page", () => {
@@ -11,4 +12,21 @@ describe("Main Page", () => {
     expect(screen.getByTestId("location")).toBeInTheDocument();
     expect(screen.getByTestId("weather")).toBeInTheDocument();
   });
+
+  it("Check if dataTime is in correct format", () => {
+    const setDateTime = jest.fn()
+    render(<Form setDateTime={setDateTime} />);
+
+    // check if adds properly
+    const dateInput = screen.getByTestId("date");
+    const timeInput = screen.getByTestId("time");
+    const viewButton = screen.getByTestId("view");
+
+    fireEvent.change(dateInput, { target: { value: '2022-09-08' } });
+    fireEvent.change(timeInput, { target: { value: '15:59' } });
+
+    fireEvent.click(viewButton);
+
+    expect(setDateTime).toHaveBeenCalled();
+  })
 });
